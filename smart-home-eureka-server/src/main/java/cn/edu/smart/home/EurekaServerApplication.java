ecdbf53bcf2c074931067a9dc6748fc87463d524
earthchen <1791538609@qq.com>
2018-06-02 13:41:40 +0800

    服务注册中心

---
+++ smart-home-eureka-server/src/main/java/cn/edu/smart/home/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package cn.edu.smart.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心
+ *
+ * @author: EarthChen
+ * @date: 2018/06/02
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
+
